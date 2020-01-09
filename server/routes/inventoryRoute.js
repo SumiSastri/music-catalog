1b6d21fe06de2bfe8445ddb04bc03d553e81a31d
Your Name <sumisastri@gmail.com>
2020-01-09 21:46:26 +0000

    get route working

--- server/routes/inventoryRoute.js
+++ server/routes/inventoryRoute.js
@@ -4,18 +4,18 @@ const mongoose = require('mongoose');
 
 const MusicItem = require('../models/musicItem.js');
 
-// READ-DATA FROM DB-GET
+// READ-DATA FROM DB READ-GET
 
 // shift from server - import file into server
-router.get('/', (req, res) => {
-	res.send('music-inventory-api route working');
-});
-
 // router.get('/', (req, res) => {
-// 	MusicItem.find().sort({ date: -1 }).then((items) => res.json(items));
+// 	res.send('music-inventory-api route working');
 // });
 
-// CREATE/WRITE-DATA TO DB-POST
+router.get('/', (req, res) => {
+	MusicItem.find().sort({ itemName: +1 }).then((items) => res.json(items));
+});
+
+// WRITE-DATA TO DB-CREATE-POST
 router.post('/', (req, res) => {
 	let { itemName, condition, valueInUSD } = req.body;
 	let musicItemPayload = new MusicItem({
@@ -31,7 +31,7 @@ router.post('/', (req, res) => {
 		.catch((err) => res.status(422).json({ message: err }));
 });
 
-// // UPDATE-DATA IN DB BY FINDING ID-PUT
+// UPDATE-DATA BY FINDING ID-UPDATE-PUT
 router.put('/:id', (req, res, next) => {
 	MusicItem.findByIdAndUpdate({ _id: req.params.id }, req.body, {
 		new: true,
@@ -41,7 +41,7 @@ router.put('/:id', (req, res, next) => {
 	});
 });
 
-// DELETE-DATA FROM DB BY ID-DELETE
+// DELETE-DATA BY FINDING ID DELETE-DELETE
 router.delete('/:id', (req, res, next) => {
 	MusicItem.findByIdAndRemove({ _id: req.params.id }, { useFindAndModify: false }).then(function(item) {
 		res.send(item);
