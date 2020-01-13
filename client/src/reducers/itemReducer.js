2d328409984110486c429b4f29ff90be6e4018c4
Your Name <sumisastri@gmail.com>
2020-01-13 21:30:15 +0000

    imports string constants into item-reducer

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,4 +1,5 @@
 import uuid from 'uuid';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from 'client/src/actions/types.js';
 
 const initialState = {
 	items: [ { id: uuid(), name: 'Maitre Gims' }, { id: uuid(), name: 'Yousoupha' }, { id: uuid(), name: 'DJ Khalid' } ]
