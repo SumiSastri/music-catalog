cdf119f30be8f29b8bf7261b2313982b27197de7
Your Name <sumisastri@gmail.com>
2020-01-13 20:43:48 +0000

    updates readme

---
+++ client/src/components/presentational/buttons/createButton 2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Button } from 'reactstrap';
+
+const createButton = (properties) => {
+	return (
+		<Container>
+			<Button color="primary" style={{ marginBotton: '2rem' }}>
+				{' '}
+			</Button>
+		</Container>
+	);
+};
+
+export default createButton;
---
+++ client/src/components/presentational/buttons/submitButton 2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Button } from 'reactstrap';
+
+const submitButton = (properties) => {
+	return (
+		<Container>
+			<Button color="success" style={{ marginBotton: '2rem' }}>
+				{' '}
+			</Button>
+		</Container>
+	);
+};
+
+export default submitButton;
