761c7101c1809422e5da5e7c21061840b9fd163f
Your Name <sumisastri@gmail.com>
2020-01-13 21:33:12 +0000

    adds export default function in item reducer

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -4,3 +4,14 @@ import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from 'client/src/actions/types.js';
 const initialState = {
 	items: [ { id: uuid(), name: 'Maitre Gims' }, { id: uuid(), name: 'Yousoupha' }, { id: uuid(), name: 'DJ Khalid' } ]
 };
+
+export default function(state = initialState, action) {
+	switch (action.type) {
+		case GET_ITEMS:
+			return {
+				...state
+			};
+		default:
+			return state;
+	}
+}
