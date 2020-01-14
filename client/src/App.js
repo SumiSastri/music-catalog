20fd014b8cd0e19eda646ac76ca2f1cdda91bdef
Your Name <sumisastri@gmail.com>
2020-01-14 21:55:56 +0000

    adds redux compose extension

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-// libraries
 import React, { Component } from 'react';
 import AppNavbar from './components/stateful/navbar';
 import MusicCatalog from './components/stateful/musicCatalog';
 // Provider shares state across all components
 import { Provider } from 'react-redux';
 import store from './redux/store';
-// import logo from './logo.svg';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
