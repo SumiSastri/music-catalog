885aa415ea6f0befa3960a191f6733e2213b872e
Your Name <sumisastri@gmail.com>
2021-01-16 12:02:43 +0000

    adds mocks to blog-section client-side

---
+++ client/src/client-side-data/mock-data/mocks-blogItem.js
@@ -0,0 +1,23 @@
+import uuid from 'uuid';
+
+const mockBlogItems = [
+	{
+		id: uuid(),
+		title: 'Squirtle Laid an Egg',
+		body:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat'
+	},
+	{
+		id: uuid(),
+		title: 'Charmander Laid an Egg',
+		body:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat'
+	},
+	{
+		id: uuid(),
+		title: 'a Helix Fossil was Found',
+		body:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat'
+	}
+];
+export default mockMusicItems;
