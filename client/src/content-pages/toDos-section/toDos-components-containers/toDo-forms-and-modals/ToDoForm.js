e28b10d13f071436cf178977ba67d43f5f822311
Your Name <sumisastri@gmail.com>
2021-01-26 19:21:13 +0000

    refactors to hooks with redux - ToDoForm

--- client/src/content-pages/toDos-section/toDos-components-containers/toDo-forms-and-modals/ToDoForm.js
+++ client/src/content-pages/toDos-section/toDos-components-containers/toDo-forms-and-modals/ToDoForm.js
@@ -1,63 +1,37 @@
-import React, { Component } from 'react';
-import { Button } from 'reactstrap';
-import uuid from 'uuid';
-
-import { FormInputField } from '../../../common-components-and-containers/forms/FormInputField';
 // HOOKS REFACTOR
-class CreateToDoForm extends Component {
-	state = {
-		id: uuid(),
-		title: '',
-		completed: false
-	};
-	// first render - used in clear form to clear inputs
-	initialState = this.state;
+import React, { useState } from 'react';
+// import React, { Component } from 'react';
+// REDUX REFACTOR
+import { connect } from 'react-redux';
 
-	// refactor - ensure name attributes to match key of state object
-	handleChange = (event) => {
-		this.setState({
-			[event.target.name]: event.target.value
-		});
-	};
+const ToDoForm = () => {
+	[ formInputs, setFormInputs ] = useState('');
 
-	handleSubmit = (event) => {
-		event.preventDefault();
-		// do not remove - debugs and checks payload is correct
-		// console.log(`submit payload:, ${this.state.id}, ${this.state.title}, ${this.state.completed}`);
-		this.props.addToDoItem(this.state);
-		this.clearFormInputs();
-	};
+	return (
+		<div name="id" id={id}>
+			<form onSubmit={this.handleSubmit}>
+				<input
+					label="What I need to do next:"
+					className="inpt-b1m"
+					datatestid="inpt-create-to-do"
+					name="title"
+					placeholder="Add items here"
+					type="text-area"
+					value={formInputs}
+					onChange={(e) => setFormInputs(e.target.value)}
+				/>
 
-	clearFormInputs = () => {
-		this.setState(this.initialState);
-	};
-
-	render() {
-		// second render after change handler
-		const { id, title, completed } = this.state;
-		return (
-			<div name="id" id={id}>
-				<form onSubmit={this.handleSubmit}>
-					<FormInputField
-						label="What I need to do next:"
-						className="inpt-b1m"
-						datatestid="inpt-create-to-do"
-						name="title"
-						onChange={this.handleChange}
-						placeholder="Add items here"
-						type="text-area"
-						value={title}
-					/>
-					<span name="title">{title}</span>
-					<span name="completed">{completed}</span>
-					<br />
-					<Button color="info" onSubmit={this.handleSubmit}>
-						Create & Submit
-					</Button>
-				</form>
-			</div>
-		);
-	}
-}
-
-export default CreateToDoForm;
+				<span name="title">{title}</span>
+				<span name="completed">{completed}</span>
+				<br />
+				<button color="info" onSubmit={this.handleSubmit}>
+					Create & Submit
+				</button>
+			</form>
+		</div>
+	);
+};
+const mapStateToProps = (state = {});
+const mapDispatchToProps = dispatch({});
+export default connect(mapStateToProps, mapDispatchToProps)(ToDoForm);
+// export default ToDoForm;
