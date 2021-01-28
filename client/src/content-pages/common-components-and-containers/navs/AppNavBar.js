6fa9a4d20bea7b2586fa5ff986df3d96b4f91e4d
Your Name <sumisastri@gmail.com>
2021-01-28 12:49:34 +0000

    rebuilds server side

--- client/src/content-pages/common-components-and-containers/navs/AppNavBar.js
+++ client/src/content-pages/common-components-and-containers/navs/AppNavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, NavItem, Jumbotron } from 'reactstrap';
+import { Collapse, Container, Navbar, NavbarToggler, NavbarBrand, NavItem, Jumbotron } from 'reactstrap';
 import logo from '../../../assets/logo.png';
 import AddNewUserModal from '../../users-section/users-components/reg-users/AddNewUserModal';
 import LoginAuthUsersModal from '../../users-section/users-components/auth-users/LoginAuthUsersModal';
@@ -23,22 +23,24 @@ class AppNavBar extends Component {
 		return (
 			<div>
 				<Jumbotron>
-					<Navbar color="light" expand="sm" className="mb-t">
-						<NavbarToggler onClick={this.toggle} />
-						<Collapse isOpen={this.state.isOpen} navbar>
-							<NavbarBrand href="/" className="nav-image-container">
-								<div className="logo-container">
-									<img style={logoStyle} alt={'logo'} src={logo} />
-								</div>
-							</NavbarBrand>
-							<h2 className="text-muted">MERN-stack app with React-Redux</h2>
-							<NavItem>
-								<AddNewUserModal />
-							</NavItem>
-							<NavItem>
-								<LoginAuthUsersModal />
-							</NavItem>
-						</Collapse>
+					<Navbar color="light" expand="sm" className="mb5">
+						<Container>
+							<NavbarToggler onClick={this.toggle} />
+							<Collapse isOpen={this.state.isOpen} navbar>
+								<NavbarBrand href="/" className="nav-image-container">
+									<div className="logo-container">
+										<img style={logoStyle} alt={'logo'} src={logo} />
+									</div>
+								</NavbarBrand>
+								<h2 className="text-muted">MERN-stack app with React-Redux</h2>
+								<NavItem>
+									<AddNewUserModal />
+								</NavItem>
+								<NavItem>
+									<LoginAuthUsersModal />
+								</NavItem>
+							</Collapse>
+						</Container>
 					</Navbar>
 				</Jumbotron>
 			</div>
